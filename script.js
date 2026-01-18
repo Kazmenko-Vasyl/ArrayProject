7f233b1de476fb3bd6261f2d6c9861bcd41160c0
Vasyl Kazmenko <62918057+Kazmenko-Vasyl@users.noreply.github.com>
2026-01-17 21:12:59 -0500

    Removing Login

--- script.js
+++ script.js
@@ -87,29 +87,4 @@ const dispayBalance = function (movements) {
   labelBalance.textContent = `$${balance}`;
 };
 
-btnLogin.addEventListener("click", function (e) {
-  // Prevent form from submitting
-  e.preventDefault();
-
-  currentAccount = accounts.find(
-    (acc) => acc.username === inputLoginUsername.value
-  );
-  console.log(currentAccount);
-
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    // Display UI and message
-    labelWelcome.textContent = `Welcome back, ${
-      currentAccount.owner.split(" ")[0]
-    }`;
-    containerApp.style.opacity = 1;
-
-    // Clear input fields
-    inputLoginUsername.value = inputLoginPin.value = "";
-    inputLoginPin.blur();
-
-    // Update UI
-    updateUI(currentAccount);
-  }
-});
-
 dispayBalance(movements);
